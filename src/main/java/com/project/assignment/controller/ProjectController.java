53aaaf74d66d43f02f0ce48736c98983a99394c2
Steve <brodeurs@gmail.com>
2022-08-13 20:47:13 -0600

    CORS origin url moved to properties file.

--- src/main/java/com/project/assignment/controller/ProjectController.java
+++ src/main/java/com/project/assignment/controller/ProjectController.java
@@ -4,6 +4,7 @@ import com.project.assignment.dto.ProjectDto;
 import com.project.assignment.entity.Project;
 import com.project.assignment.repo.ProjectRepository;
 import com.project.assignment.service.ProjectService;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -20,6 +21,7 @@ public class ProjectController {
         this.projectService = projectService;
     }
 
+    @CrossOrigin(origins = "${project.controller.cors.url}")
     @GetMapping("/projects")
     List<ProjectDto> all() {
         return projectService.getProjects();
