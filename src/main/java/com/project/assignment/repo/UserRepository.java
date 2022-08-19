71d017682b1782624d8fa157c8a34c806f839b65
Steve <brodeurs@gmail.com>
2022-08-19 11:27:13 -0600

    Added native query comment.

--- src/main/java/com/project/assignment/repo/UserRepository.java
+++ src/main/java/com/project/assignment/repo/UserRepository.java
@@ -9,6 +9,8 @@ import java.util.Set;
 
 public interface UserRepository extends JpaRepository<User, Long> {
 
+    // Native query:
+    // select * from project_user join project_users on  project_users.project_id = 7 and project_users.user_id = project_user.user_id;
     @Query(value = "select  p.users from Project p join p.users u  where p.id=:project_id")
     Set<User> findUsersByProjectId(@Param("project_id") Long project_id);
 
